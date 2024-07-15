928b6c3973c65725d6edd31111245bf4a4f5099f
Arijeet Mukherjee <ari123acess@gmail.com>
2024-07-16 01:08:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,28 @@ const cors = require('cors');
 
 app.use(cors({origin : "*"}));
 
-
 app.post('/api', (req, res) => {
-    res.send('Hello World ${req.body}' + req.body);
-    console.log('Hello World ${req.body}' + req.body);
+    res.send('Hello World.. Got event' + req.body);
+    const githubEvent = req.headers['x-github-event'];
+    
+      if (githubEvent === 'issues') {
+        const data = request.body;
+        const action = data.action;
+        if (action === 'opened') {
+          console.log(`An issue was opened with this title: ${data.issue.title}`);
+        } else if (action === 'closed') {
+          console.log(`An issue was closed by ${data.issue.user.login}`);
+        } else {
+          console.log(`Unhandled action for the issue event: ${action}`);
+        }
+      } else if (githubEvent === 'ping') {
+        console.log('GitHub sent the ping event');
+      } else {
+        console.log(`Unhandled event: ${githubEvent}`);
+      }
     return;
 });
+
 app.get('/api', (req, res) => {
     res.send('Hello World ${req.body}' + req.body);
     return;
